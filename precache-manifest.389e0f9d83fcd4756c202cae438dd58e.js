25a97fa8d1b195767b643fe9eee3322160aa40a6
zhangzhiguo <zhangzhiguo010@outlook.com>
2018-10-30 09:49:33 +0800

    Updates

---
+++ precache-manifest.389e0f9d83fcd4756c202cae438dd58e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6bb10c6e24100119f5c1f88e5c64f5e",
+    "url": "/resume-2018/static/media/1层.d6bb10c6.jpg"
+  },
+  {
+    "revision": "298df5711ec66e9a8415",
+    "url": "/resume-2018/static/css/main.33dfe5f2.chunk.css"
+  },
+  {
+    "revision": "854ce757d2f1578fe007",
+    "url": "/resume-2018/static/js/1.854ce757.chunk.js"
+  },
+  {
+    "revision": "da1398b09d7e69446d3d",
+    "url": "/resume-2018/static/js/runtime~main.da1398b0.js"
+  },
+  {
+    "revision": "c44dce0af6d0e7131e459db35297a342",
+    "url": "/resume-2018/static/media/photo.c44dce0a.jpg"
+  },
+  {
+    "revision": "298df5711ec66e9a8415",
+    "url": "/resume-2018/static/js/main.298df571.chunk.js"
+  },
+  {
+    "revision": "54d7fb76b4198258e12371ee589dc9c5",
+    "url": "/resume-2018/static/media/2层.54d7fb76.jpg"
+  },
+  {
+    "revision": "af35bea16810bf6bb76424426b229808",
+    "url": "/resume-2018/static/media/3层.af35bea1.jpg"
+  },
+  {
+    "revision": "053654b552a368c705e2fb319aaa5c31",
+    "url": "/resume-2018/static/media/4层.053654b5.jpg"
+  },
+  {
+    "revision": "bf6e10c6bffa08e6772fe41939d1aab0",
+    "url": "/resume-2018/static/media/topBarBgImg.bf6e10c6.jpg"
+  },
+  {
+    "revision": "05f2ed0dee861ed99e7d0c4f85bc245d",
+    "url": "/resume-2018/index.html"
+  }
+];
